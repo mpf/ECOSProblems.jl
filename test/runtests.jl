ee16a2c2b420a47e5bb0487ef5b9dbf10ad26281
Michael Friedlander <michael@friedlander.io>
2020-01-15 15:30:50 -0800

    Cleanup test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,17 +3,14 @@ using LinearAlgebra
 using Test
 
 function dual_inf(x, z, bl, bu)
-    dInf = 0.0
-    jInf = 0
+    dInf = 0.0; jInf = 0
     for j = 1:n
-	      blj = bl[j]
-	      buj = bu[j]
-	      if blj < buj
+	      if bl[j] < bu[j]
 	          dj = z[j]
 	          if dj > 0
-		            dj =  dj * min(x[j] - blj, 1.0)
+		            dj =  dj * min(x[j] - bl[j], 1.0)
 	          elseif dj < 0
-		            dj = -dj * min(buj - x[j], 1.0)
+		            dj = -dj * min(bu[j] - x[j], 1.0)
             end
 	          if dInf < dj
 		            dInf = dj
@@ -26,17 +23,16 @@ end
 
 @testset "ECOSProblems.jl" begin
     atol = 1e-6
-    n = 2; m = 300
+    n = 3; m = 100
     A = randn(m, n)
     x0 = rand(n)
     b = A*x0
     u = rand(n)
-    x, r, rNorm = lsbox(A, b, u, verbose=true)
-
-    g = -A'r
+    x, r, rNorm = lsbox(A, b, u, verbose=false)
     @test isapprox(norm(r), rNorm, atol=atol)
     @test isapprox(norm(b-A*x), norm(r), atol=atol)
-    dInf, jInf = @enter dual_inf(x, g, zeros(n), u)
-    println("dual Inf = $dInf, var = $jInf")
+    dInf, jInf = dual_inf(x, -(A'r), zeros(n), u)
+    @test dInf ≤ 1e-3
+    # println("dual Inf = $dInf, var = $jInf, $(g[jInf])   $(u[jInf]-x[jInf]) ")
 end
 
